d480e58951fcce56b5e61e1e5d966e7e8d2b410e
Tanbir <tanbirka@thoughtworks.com>
2014-01-20 18:54:04 +0530

    sh file edited

--- run-scripts/tk_eval.sh
+++ run-scripts/tk_eval.sh
@@ -1,1 +1,1 @@
-java -cp ${EVALUATOR_HOME}/lib/expression-evaluator.jar tanbirka.expressionEvaluator.cli.EvalMain "$@"
+java -cp ${EVALUATOR_HOME}/lib/expression-evaluator.jar tanbirka.expressionEvaluator.cli.EvaluatorMain "$@"
