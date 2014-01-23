77d573cffcca4d38ae1a2ede9f6fb34a58b1cc0d
Tanbir <tanbirka@thoughtworks.com>
2014-01-23 14:36:52 +0530

    version 0.3 works

--- src/tanbirka/expressionEvaluator/api/Evaluator.java
+++ src/tanbirka/expressionEvaluator/api/Evaluator.java
@@ -4,14 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Evaluator {
-    public int evaluateExpression(String expression) {
 
-        String[] values = expression.split(" ");
+    public int evaluateExpression(String expression) {
+        String expressionToEval = expression;
+        if (expression.contains("("))
+            expressionToEval = solveBrackets(expression);
+        return evaluate(expressionToEval.toString());
+    }
+
+    private int evaluate(String expression) {
+        String[] values = expression.trim().split(" ");
+
         List<String> operators = new ArrayList<String>();
         List<Integer> operands = new ArrayList<Integer>();
-
         operators.add("+");
-        getOperandsAndOperator(values, operators, operands);
+        separateOperandsAndOperator(values, operators, operands);
 
         Operations operationsMap = new Operations();
         int result = 0;
@@ -24,9 +31,10 @@ public class Evaluator {
 
             result = operationsMap.operate(operator, operand1, operand2);
         }
-        return  result;
+        return result;
     }
-    private void getOperandsAndOperator(String[] values, List<String> operators, List<Integer> operands) {
+
+    private void separateOperandsAndOperator(String[] values, List<String> operators, List<Integer> operands) {
         for (String value : values) {
             try {
                 operands.add(Integer.parseInt(value));
@@ -36,9 +44,23 @@ public class Evaluator {
         }
     }
 
+
+    private String solveBrackets(String expression) {
+        String expressionInBracket, expressionWithoutBrackets;
+        int startIndex =0;
+        int endIndex =0;
+        for (int index = 0; index < expression.length(); index++) {
+            if ('(' == expression.charAt(index)) {
+                startIndex = index;
+            }
+            if (')' == expression.charAt(index)) {
+                endIndex = index;
+                break;
+            }
+        }
+        expressionInBracket = expression.substring(startIndex + 1, endIndex);
+        int resultOfBracket = this.evaluate(expressionInBracket);
+        expressionWithoutBrackets = expression.replace('(' + expressionInBracket + ')', Integer.toString(resultOfBracket));
+        return expressionWithoutBrackets;
+    }
 }
-
-
-
-
-
